eefa0b5554c3b6f2b38fcc1f79981ffcec7b6110
Nattapong-saethao <nattapongsaethao.nan@gmail.com>
2025-02-04 13:01:00 +0700

    Update db_connection.php

--- page/php_script/db_connection.php
+++ page/php_script/db_connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root"; // ค่าเริ่มต้นของ XAMPP คือ "root"
-$password = ""; // ค่าเริ่มต้นของ XAMPP คือค่าว่าง
-$dbname = "coffe_shop"; // ชื่อฐานข้อมูลของคุณ
+$servername = "dpg-cuem0q56l47c73eu2ja0-a";
+$username = "coffe_shop_jv6p_user"; // ค่าเริ่มต้นของ XAMPP คือ "root"
+$password = "WXWfnJt0cmZZv1o5rJPCGchwNiiwPZLK"; // ค่าเริ่มต้นของ XAMPP คือค่าว่าง
+$dbname = "coffe_shop_jv6p"; // ชื่อฐานข้อมูลของคุณ
 
 // สร้างการเชื่อมต่อ
 $conn = new mysqli($servername, $username, $password, $dbname);
